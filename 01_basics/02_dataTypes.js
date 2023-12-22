1b65f3e2a2e16f6640940fda2445640215fe27c4
Ankit-Shaw1 <137321171+Ankit-Shaw1@users.noreply.github.com>
2023-12-22 04:52:24 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  // we are using nodejs, not browser so it will not work
+
+console.log(3
+    +
+    3) //code readability should be high
+
+    console.log("Ankit");
+
+    let name="Ankit";
+    let age=18;
+    let isLoggedIn=false;
+    let state;
+
+    //number => 2 to the power 53
+    //bigint
+    //string => ""
+    //boolean= true/false
+    //null => standalone value
+    //undefined => values are not assigned yet
+    //symbol
+
+    //object
+
+    console.log(typeof null);  //object
+    console.log(typeof undefined);  //undefined

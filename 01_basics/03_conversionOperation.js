d83bf439ed0fd3597854b05525c93af08cd5b0d9
Ankit-Shaw1 <137321171+Ankit-Shaw1@users.noreply.github.com>
2023-12-23 10:45:39 +0000

    Comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,8 +25,27 @@ console.log(booleanIsLoggedIn);
 //"" =>false
 // "hitesh" =>true
 
-let somenumber=33;
+let somenumber = 33;
 
-let stringnumber=String(somenumber);
+let stringnumber = String(somenumber);
 console.log(stringnumber);
 console.log(typeof (stringnumber));
+
+
+//  ******************Operation************* 
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " Ankit"
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log(1+"2"+"2");
+console.log(1+2+"2");
+console.log("1"+2+3);

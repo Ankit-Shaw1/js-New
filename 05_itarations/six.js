fb958037aebcc268d85db48ed45109aa87e28df6
Ankit-Shaw1 <137321171+Ankit-Shaw1@users.noreply.github.com>
2024-02-06 06:27:42 +0000

    filter

---
+++ 05_itarations/six.js
@@ -0,0 +1,14 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const value = coding.forEach((item) => {
+//     console.log(item);                                 Note---for each return nhi krta kuchh v
+// })
+
+// console.log(value);
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter((num)=> num>4)
+
+console.log(newNums);

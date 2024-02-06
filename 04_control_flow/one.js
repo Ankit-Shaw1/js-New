9c1f01aca831f754c7fdc55bb95113f8be084af7
Ankit-Shaw1 <137321171+Ankit-Shaw1@users.noreply.github.com>
2024-02-06 03:21:54 +0000

    control flows

---
+++ 04_control_flow/one.js
@@ -0,0 +1,37 @@
+// if
+
+// const isUserloggedIn = true
+
+// if (2=="2") {
+//     console.log("executed");
+// }
+
+// if (2==="2") {                   // datatype will be checked
+//     console.log("executed");
+// }
+
+
+const temp = 41
+
+// if(temp===40)
+// {
+//     console.log("less than 40");
+// }else{
+//     console.log("temp is greater than 40");
+// }
+
+const score = 200
+
+// if(score>100)
+// {
+//     const power="fly"
+//     console.log(`User power: ${power}`);
+// }
+
+const balance = 1000
+
+if (balance > 500) console.log("test");
+
+
+
+

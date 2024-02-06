8dac4430e441122fc2ecefe95430d570e780a6af
Ankit-Shaw1 <137321171+Ankit-Shaw1@users.noreply.github.com>
2024-02-06 06:13:27 +0000

    for each

---
+++ 05_itarations/five.js
@@ -0,0 +1,38 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function (val){
+//         console.log(val);
+// })
+
+// coding.forEach((val) => {
+//     console.log(val);
+// })
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+coding.forEach((item,index,arr)=>{
+    // console.log(item,index,arr);
+})
+
+const myCoding =[
+    {
+        languageName:"javascript",
+        languagefilename:"java"
+    },
+    {
+        languageName:"java",
+        languagefilename:"java"
+    },
+    {
+        languageName:"python",
+        languagefilename:"py"
+    },
+]
+
+myCoding.forEach((item)=>{
+        console.log(item.languageName);
+})
